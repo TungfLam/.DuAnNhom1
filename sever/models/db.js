cd0149c68dac0f4ebcd63441f3b73f218533cd6e
Tung Lam <lamptph20282@fpt.edu.vn>
2023-05-25 16:45:41 +0700

    update lại url sever

--- sever/models/db.js
+++ sever/models/db.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
-// mongoose.connect('mongodb://127.0.0.1:27017/testdatabaseDUANNHOM1') 
+mongoose.connect('mongodb://127.0.0.1:27017/testdatabaseDUANNHOM1',{useNewUrlParser:true,useUnifiedTopology:true}) 
 
-mongoose.connect('mongodb+srv://saoooo2pro:1WA4zP1gtAiL4fA1@databasenhom1.p8zny2e.mongodb.net/?retryWrites=true&w=majority/databaseNhom1',{
-    useNewUrlParser:true,useUnifiedTopology:true
-}) 
+// mongoose.connect('mongodb+srv://saoooo2pro:1WA4zP1gtAiL4fA1@databasenhom1.p8zny2e.mongodb.net/?retryWrites=true&w=majority/databaseNhom1',{useNewUrlParser:true,useUnifiedTopology:true}) 
     .catch((err) => {
         console.log('loi ket noi CSDL');
         console.log(err);
